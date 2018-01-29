21a605e1d2f24e0b8ec40e88795bf0b2998382aa
Jeswanth <jkodali7@gmail.com>
2018-01-29 15:37:34 -0500

    update date_time feild

--- currentweather/models.py
+++ currentweather/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # The models file is used for creating database schemas that you can use to query the database
 class Weather(models.Model):
-    temprature = models.CharField(max_length=100)
+    temperature = models.CharField(max_length=100)
     wind_speed = models.CharField(max_length=100)
     humidity = models.CharField(max_length=100)
     pressure = models.CharField(max_length=100)
-    dates = models.CharField(max_length=100)
+    date_time = models.DateTimeField()
 
     def __set__(self):
-        return self.temperature + ' - ' + self.wind_speed + ' - ' + self.humidity + ' - ' + self.pressure + ' - ' + self.dates
+        return self.temperature + ' - ' + self.wind_speed + ' - ' + self.humidity + ' - ' + self.pressure + ' - ' + self.date_time
