417e7fe668c1e34b25e7c727241e2bec83bdcda8
Brian Atiyeh <btatiyeh@gmail.com>
2018-01-29 15:34:06 -0500

    Changed mysql user to be weatherstation specific

--- weatherstation/settings.py
+++ weatherstation/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'weatherstation',
-        'USER': 'root',
-        'PASSWORD': 'pass1234',
+        'USER': 'weatherstation',
+        'PASSWORD': 'ws1234',
         'HOST': 'localhost',
         'PORT': '3306',
     }
