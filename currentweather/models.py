8d67889ecb7a150ad813250fad7ee5240e7e5c5c
Jeswanth <jkodali7@gmail.com>
2018-01-29 22:26:34 -0500

    inserted id for database

--- currentweather/models.py
+++ currentweather/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # The models file is used for creating database schemas that you can use to query the database
 class Weather(models.Model):
+    id = models.IntegerField(primary_key=True)
     temperature = models.CharField(max_length=100)
     wind_speed = models.CharField(max_length=100)
     humidity = models.CharField(max_length=100)
@@ -10,4 +11,3 @@ class Weather(models.Model):
 
     def __set__(self):
         return self.temperature + ' - ' + self.wind_speed + ' - ' + self.humidity + ' - ' + self.pressure + ' - ' + self.date_time  #this shows data clear in shell
-    
