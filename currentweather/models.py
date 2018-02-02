fb82d8ae3d22d1ae42db097e659027040d025b98
Brandon <brandokaw428@aim.com>
2018-02-01 20:12:13 -0500

    merging brandon branch to master

--- currentweather/models.py
+++ currentweather/models.py
@@ -10,6 +10,7 @@ class StationWeather(models.Model):
     pressure = models.FloatField(blank=True, null=True)
     humidity = models.FloatField(blank=True, null=True)
 
+
 class ApiWeather(models.Model):
     id = models.IntegerField(primary_key=True)
     temperature = models.CharField(max_length=100)
@@ -20,3 +21,10 @@ class ApiWeather(models.Model):
 
     def __set__(self):
         return self.temperature + ' - ' + self.wind_speed + ' - ' + self.humidity + ' - ' + self.pressure + ' - ' + self.date_time  #this shows data clear in shell
+
+class UserAccount(models.Model):
+    userid = models.AutoField(primary_key=True)
+    email = models.EmailField()
+    password = models.CharField(max_length=16)
+    phone = models.CharField(max_length=10)
+
