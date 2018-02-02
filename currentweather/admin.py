b2f022b63699919f650b8df483c4cd618e18dc6b
Brian Atiyeh <btatiyeh@gmail.com>
2018-02-01 20:20:22 -0500

    Fixed admin model imports

--- currentweather/admin.py
+++ currentweather/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Weather
+from .models import ApiWeather
+from .models import StationWeather
 # Register your models here.
 
 admin.site.register(ApiWeather)
