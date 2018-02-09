8aca2238fc248f5cd5802cd61756324262e1a5e8
Brian Atiyeh <btatiyeh@gmail.com>
2018-02-09 00:44:42 -0500

    Removed Django project files

--- currentweather/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ApiWeather
-from .models import StationWeather
-# Register your models here.
-
-admin.site.register(ApiWeather)
-admin.site.register(StationWeather)
--- currentweather/static/js/test.js
+++
@@ -1,9 +0,0 @@
-$('#post-form').on('submit',function(event){
-    event.preventDefault();
-    console.log("form submitted!")
-    create_post();
-});
-
-function create_post(){
-    console.log("create post is working!")
-};
