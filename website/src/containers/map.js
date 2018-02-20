0674aec9439d51036d2e2407f7e42c28658d3845
Jeswanth <jkodali7@gmail.com>
2018-02-20 16:21:33 -0500

    reverted changes

--- website/src/containers/map.js
+++ website/src/containers/map.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
-import Navigation from '../components/navigation.js';
-import VerifyLoggedIn from '../components/verifyLoggedIn.js'
++import Navigation from '../components/navigation.js';
++import VerifyLoggedIn from '../components/verifyLoggedIn.js'
+import StationMap from '../components/stations/stationMap.js';
 
+    class Map extends Component {
+        render() {
+            return (
+                <StationMap></StationMap>
 
-class Map extends Component {
-  render() {
-    return (
-      <div className='MapPage'>
-        <VerifyLoggedIn/>
-        <Navigation/>
-        This is the map page
-      </div>
-    );
-  }
-}
-
-export default Map;
+                    class Map extends Component {
+                      render() {
+                                return (
+                                      <div className='MapPage'>
+                                            <VerifyLoggedIn/>
+                                            <Navigation/>
+                                            This is the map page
+                                         </div>
+                            );
+                        }
+                    }
