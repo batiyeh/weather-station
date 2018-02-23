b1a5308377e5bdda12438cb808705a706a07110a
Brian Atiyeh <btatiyeh@gmail.com>
2018-02-23 13:32:13 -0500

    Fixed logout redirect bug

--- website/src/components/navigation.js
+++ website/src/components/navigation.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../styles/navbar.css';
 import logo from '../images/space-satellite-dish-512x512.png';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import {
     Navbar,
     NavbarBrand,
@@ -17,7 +17,8 @@ class Navigation extends Component {
         super(props);
         this.toggle = this.toggle.bind(this);
         this.state = {
-            dropdownOpen: false
+            dropdownOpen: false,
+            redirect: false
         }
     }
 
@@ -27,49 +28,60 @@ class Navigation extends Component {
         })
     }
 
-    logout(){
-        fetch('/api/user/logout', { method:"post", credentials: 'same-origin' });
+    logout = async() => {
+        var response = await fetch('/api/user/logout', {method: 'post', credentials: 'include'})
+        var body = await response.json();
+        this.setState({
+            redirect: true
+        })
+        return body;
     }
 
     render() {
-        return (
-            <div>
-                <Navbar className="react-nav" color="faded" light expand="md">
-                    <NavbarBrand href="/">
-                        <Link to={'/'} className='nav-link'><img src={logo} width="30" height="30" alt=""></img></Link>
-                    </NavbarBrand>
-                    <Nav>
-                        <NavItem>
-                            <Link to={'/'} className='nav-link'>stations</Link>
-                        </NavItem>
-                        <NavItem>
-                            <Link to={'/map'} className='nav-link'>map</Link>
-                        </NavItem>
-                        <NavItem>
-                            <Link to={'/historical'} className='nav-link'>historical</Link>
-                        </NavItem>
-                    </Nav>
-                    <Nav className="ml-auto" navbar>
-                        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} nav inNavbar>
-                            <DropdownToggle nav caret>
-                            username
-                            </DropdownToggle>
-                            <DropdownMenu className="user-menu" right>
-                                <DropdownItem>
-                                    <Link to={'/profile'} className='nav-link nav-link-dark'>profile</Link>
-                                </DropdownItem>
-                                <DropdownItem>
-                                    <Link to={'/alerts'} className='nav-link nav-link-dark'>alerts</Link>
-                                </DropdownItem>
-                                <DropdownItem>
-                                    <a onClick={this.logout} className='nav-link nav-link-dark'>logout</a>
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </Dropdown>
-                    </Nav>
-                </Navbar>
-            </div>
-        );
+        if(this.state.redirect) {
+            return <Redirect to='/user/login'/>;
+        }
+
+        else{
+            return (
+                <div>
+                    <Navbar className="react-nav" color="faded" light expand="md">
+                        <NavbarBrand href="/">
+                            <Link to={'/'} className='nav-link'><img src={logo} width="30" height="30" alt=""></img></Link>
+                        </NavbarBrand>
+                        <Nav>
+                            <NavItem>
+                                <Link to={'/'} className='nav-link'>stations</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to={'/map'} className='nav-link'>map</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to={'/historical'} className='nav-link'>historical</Link>
+                            </NavItem>
+                        </Nav>
+                        <Nav className="ml-auto" navbar>
+                            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} nav inNavbar>
+                                <DropdownToggle nav caret>
+                                username
+                                </DropdownToggle>
+                                <DropdownMenu className="user-menu" right>
+                                    <DropdownItem>
+                                        <Link to={'/profile'} className='nav-link nav-link-dark'>profile</Link>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <Link to={'/alerts'} className='nav-link nav-link-dark'>alerts</Link>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <a onClick={this.logout} className='nav-link nav-link-dark'>logout</a>
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                        </Nav>
+                    </Navbar>
+                </div>
+            );
+        }
   }
 }
 
