f1c5e032d5b401d538022d5f92c91c156a167d15
fa4269 <fa4269@wayne.edu>
2018-02-24 15:34:06 -0500

    Created modal for user changing password on profile page

--- website/src/components/profileForm.js
+++ website/src/components/profileForm.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { Input, Button, Card, CardText, CardTitle, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import '../styles/profile.css';
 
 class ProfileForm extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            modal: false
+        }
+
+        this.toggleChangePassword = this.toggleChangePassword.bind(this);
+    }
+    toggleChangePassword(){
+        this.setState({
+            modal: !this.state.modal
+        })
+    }
+
     render(){
         return(
             <div className='profile-container'>
+             <Modal isOpen={this.state.modal} toggle={this.toggleChangePassword}>
+                <ModalHeader toggle={this.toggleChangePassword}>Change Password</ModalHeader>
+                <ModalBody>
+                    <form id='passwordForm' action='/api/user/editPassword' method='post'>
+                        <input id='currPass' name='currPass' type='password' className='form-control' placeholder='Current Password'/>
+                        <input id='newPass' name='newPass' type='password' className='form-control' placeholder='New Password'/>
+                    </form>
+                </ModalBody>
+                <ModalFooter>
+                        <Button color="primary" className="primary-themed-btn" type='submit'>Change Password</Button>{' '}
+                        <Button color="secondary" onClick={this.toggleChangePassword}>Cancel</Button>
+                </ModalFooter>
+             </Modal>
                 <div id='profile'>
                     <form id='profileForm' action='/api/user/editProfile/'method='post'>
                         <div className='profile-info mb-3'>
@@ -17,8 +45,10 @@ class ProfileForm extends Component {
                         </div>
                         <div className='row'>
                             <div className='col-6'>
+                                <button type='button' color='primary' className="btn btn-default btn-block profile-btn" onClick={this.toggleChangePassword}>Change Password</button>
                                 <button type='submit' className='btn btn-default btn-block profile-btn'>Save Changes</button>
                             </div>
+                            
                         </div>
                     </form>
                 </div>
