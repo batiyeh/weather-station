4b4267890c432457896f451a2d7186cd99e00b46
Brian Atiyeh <btatiyeh@gmail.com>
2018-03-03 13:00:44 -0500

    Client code now requires user input to verify the API key

--- client/client.py
+++ client/client.py
@@ -3,6 +3,7 @@ import requests
 import time
 import json
 import datetime
+from pathlib import Path
 try:
     import Adafruit_DHT
 except:
@@ -12,11 +13,41 @@ try:
 except:
     pass
 
+def getApiKey():
+    keyFile = Path("./.api-key.txt")
+    if keyFile.is_file():
+        with open('./.api-key.txt', 'r') as f:
+            key = f.readline()
+            return key
+    else:
+        verified = False
+        while(not verified):
+            key = input("Enter your API Key: ")
+            try:
+                print("Verifying key...")
+                r = requests.post('http://localhost:5000/api/weather/verifyKey', data = {"key": key})
+                if (r.status_code == 200):
+                    print("Key Verified.")
+                    f = open('./.api-key.txt', 'w')
+                    f.write(key)
+                    f.close()
+                    verified = True
+                elif (r.status_code == 400):
+                    print("Invalid API key. Please try again.")
+                else:
+                    print("Something went wrong with the server.")
+            except:
+                print("Lost connection to server.")
+                pass
+
+    return key
+
+
 if __name__ == '__main__':
     temperature = 0
     pressure = 0
     humidity = 0
-    apikey = 'bf1df01a9f633b9fece2'
+    apikey = getApiKey()
     pin = 14
     
     try:
@@ -36,19 +67,15 @@ if __name__ == '__main__':
                 longitude = agps_thread.data_stream.lon
             # For running from a laptop and we just need fake data
             except:
-
                 latitude = "n/a"
                 longitude = "n/a"
-
-                latitude = "n/a"
-                longitude = "n/a"
-            #
+            
+            # Attempt to retrieve humidity + temperature
             try:
                 humidity, temperature = Adafruit_DHT.read(Adafruit_DHT.AM2302, pin)
             except:
                 temperature += 5
                 humidity += 5
-
 
             pressure += 5
 
