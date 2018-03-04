cd68d818f9c9a5218ea7d0aa830ddf0ee6cb4d66
fa4269 <fa4269@wayne.edu>
2018-03-04 13:59:01 -0500

    Email alerts for above/below threshholds working

--- scripts/sendAlerts.js
+++ scripts/sendAlerts.js
@@ -1,11 +1,12 @@
 const knex = require('knex')(require('../knexfile'))
+const nodemailer = require('nodemailer');
 
 getAlerts = async () =>{
     var alerts = await knex('alerts')
-    .select('alerts.alert_id', 'alerts.station_name', 'alerts.type', 'alerts.keyword', 'alerts.last_triggered', 'alertvalues.value', 'alertmethods.method', 'alerts.username')
+    .select('alerts.alert_id', 'alerts.station_name', 'alerts.type', 'alerts.keyword', 'alerts.last_triggered', 'alertvalues.value', 'alertmethods.method', 'alerts.username', 'users.email')
     .leftJoin('alertvalues', 'alerts.alert_id', '=', 'alertvalues.alert_id')
     .leftJoin('alertmethods', 'alerts.alert_id', '=', 'alertmethods.alert_id')
-
+    .leftJoin('users', 'alerts.username', '=', 'users.username')
     return alerts;
 }
 getWeather = async () => {
@@ -16,23 +17,82 @@ getWeather = async () => {
     return weather;
 }
 comparison = async () => {
-    var email = [];
-    var sms = [];
-    var webpage = [];
+    var triggered = []
     var alerts = await getAlerts();
     var weather = await getWeather();
-    console.log(weather);
     
     alerts.map(alerts =>{
         weather.map(weather =>{
-            console.log(weather[alerts.type]);
-
+            if(alerts.keyword === 'above'){
+                if(weather[alerts.type] > alerts.value){
+                    triggered.push(alerts);
+                }
+            }
+            else if(alerts.keyword === 'between'){
+                
+            }
+            else if(alerts.keyword === 'below'){
+                if(weather[alerts.type] > alerts.value){
+                    triggered.push(alerts);
+                }
+            }
         })
     })
+
+    triggered.map(triggered =>{
+        if(triggered.method === 'email'){
+            sendEmail(triggered, weather);
+        }
+        else if(triggered.method === 'sms'){
+            sendSMS(triggered, weather);
+        }
+        else if(triggered.method === 'webpage'){
+            sendWebpage(triggered, weather);
+        }
+    })
 }
-above = async () =>{
+sendEmail = async (triggered, weather) =>{
+    var triggeredStation = null;
+    weather.map(weather=>{
+        if(weather.station_name === triggered.station_name){
+            triggeredStation = weather;
+        }
+    })
+    console.log(triggeredStation)
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            //Find better way to store user and pass for whole system.
+            user: 'WStationTestdod@gmail.com',
+            pass: 'wayne123'
+        }
+    });
+    var mailOptions = {
+        to: triggered.email,
+        from: 'wstationtestdod@gmail.com',
+        subject: 'Inclement weather alert!',
+        text: 'You are receiving this message because the follwing alert was triggered:\n\n'+
+        'The ' + triggered.type + ' is ' + triggered.keyword + ' ' + triggered.value + ' at station: ' + triggered.station_name + '\n\n'+
+        'The current weather at ' + triggered.station_name + ' is: \n\n'+
+        'Temperature: ' + triggeredStation.temperature + '\n' +
+        'Pressure: ' + triggeredStation.pressure + '\n' +
+        'Humidity: ' + triggeredStation.humidity + '\n'
+        
+    };
+    transporter.sendMail(mailOptions,function(err){
+        //Alert user email has been sent
+        done(err, 'done');
+    });
+}
+sendSMS = async (triggered, weather) => {
 
 }
+sendWebpage = async (triggered, weather) => {
+
+}
+
 comparison();
 
 
