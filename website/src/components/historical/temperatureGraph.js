62119d68e114ee36aa2dc3bd60091c850382a10a
Brian Atiyeh <btatiyeh@gmail.com>
2018-03-05 15:16:31 -0500

    Fixed historical line not showing up

--- website/src/components/historical/temperatureGraph.js
+++ website/src/components/historical/temperatureGraph.js
@@ -17,7 +17,6 @@ import { timeFormat as d3timeFormat,
 class TemperatureGraph extends Component{
     constructor(props){
         super(props);
-        console.log(props);
         this.state = {
             data: [],
             height: this.props.height,
@@ -26,21 +25,24 @@ class TemperatureGraph extends Component{
             width: this.props.width,
             margin: {top: 20, right: 20, bottom: 30, left: 50},
         }
-        console.log(this.state.data);
     }
+    
     componentWillMount(){
         this.getTemp();
     }
+
     getTemp = async () => {
         var tempData;
         const response = await fetch('/api/weather/temp/');
         const body = await response.json();
         if (response.status !== 200) throw Error(body.message);
         if (body.temp) tempData = body.temp;
-        this.setState({data: tempData});
+        this.setState({
+            data: tempData,
+            selectX: datum => new Date(datum.created_at),
+            selectY: datum => parseFloat(datum.temperature)
+        });
     };
-
-
 
     render(){
         console.log(this.state.data);
@@ -48,13 +50,13 @@ class TemperatureGraph extends Component{
         var dayExtent = d3ArrayExtent(this.state.data, function(d) {return d3timeFormat(d.created_at).parse});
         console.log(dayExtent);
         const xScale = d3ScaleTime()
-            //.domain(0, dayExtent)
+            .domain(d3ArrayExtent(this.state.data, this.state.selectX))
             .range([0, 1000]);
 
         // Our y axis should just have a linear scale.
         // Our y domain will be the extent of y values (numbers) in our data set.
         const yScale = d3ScaleLinear()
-            .domain([0,100])
+            .domain(d3ArrayExtent(this.state.data, this.state.selectY))
             .range([this.state.height, 0]);
 
         // These two functions select the scaled x and y values (respectively) of our data.
@@ -71,8 +73,8 @@ class TemperatureGraph extends Component{
             .ticks(5);
         // Create a d3Line factory for our scales.
         const tempLine = d3Line()
-            .x(function (d){return (d.created_at)})
-            .y(function (d){return (d.temperature)});
+            .x(function (d){return xScale(new Date(d.created_at))})
+            .y(function (d){return yScale(parseFloat(d.temperature))});
 
         //Set the svg for the graph to be rendered
         const graph = d3Select("svg")
@@ -88,8 +90,7 @@ class TemperatureGraph extends Component{
             .attr("fill", 'none');
 
         return(
-            <svg ref={node => this.node = node}
-                 width={1000} height={500} display="center" >
+            <svg ref={node => this.node = node} width={1000} height={500} display="center" >
             </svg>
         );
     }
