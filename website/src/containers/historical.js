066b04a873668b4c8bdb10d703fe4a7c6fb804ed
Trevor Malarkey <35282170+Tmalarkey@users.noreply.github.com>
2018-03-07 12:46:43 -0500

    Working on historical graph filter button

--- website/src/containers/historical.js
+++ website/src/containers/historical.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Alert } from 'reactstrap';
 import VerifyLoggedIn from '../components/verifyLoggedIn.js'
 import HistoricalContainer from '../components/historical/historicalContainer'
 import '../styles/App.css';
@@ -11,8 +12,12 @@ class Historical extends Component {
                 <VerifyLoggedIn/>
                 <div id="historical-container">
                     <h2 className="page-title">Historical Graph</h2>
-
                     <HistoricalContainer></HistoricalContainer>
+                    <div class="filter">
+                        <div class="col-sm-8">
+                            <Button type='button' className="btn btn-secondary btn-block">Filter</Button>
+                        </div>
+                    </div>
                  </div>
             </div>
         );
