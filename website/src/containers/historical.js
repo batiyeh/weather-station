59fab55e0150906f3e1d6a1c75d0e8e02d5b9502
Trevor Malarkey <35282170+Tmalarkey@users.noreply.github.com>
2018-03-07 14:32:37 -0500

    Fixing the css for the filter button to make it above the graph

--- website/src/containers/historical.js
+++ website/src/containers/historical.js
@@ -10,13 +10,13 @@ class Historical extends Component {
         return (
             <div className='NavBar'>
                 <VerifyLoggedIn/>
-                <div id="historical-container">
+                <div id="historical-container container">
                     <h2 className="page-title">Historical Graph</h2>
-                    <HistoricalContainer></HistoricalContainer>
-                    <div class="filter">
-                        <div class="col-sm-8">
-                            <Button type='button' className="btn btn-secondary btn-block">Filter</Button>
-                        </div>
+                    <div className="filter row">
+                        <Button type='button' className="btn btn-primary">Filter</Button>
+                    </div>
+                    <div className="row">
+                        <HistoricalContainer></HistoricalContainer>
                     </div>
                  </div>
             </div>
