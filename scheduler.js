809cc8152a529a5c6b1252305f26d3219c70f182
Brian Atiyeh <btatiyeh@gmail.com>
2018-04-09 20:57:18 -0400

    Connection schedule checks every 10 seconds now

--- scheduler.js
+++ scheduler.js
@@ -6,21 +6,9 @@ module.exports =  {
     // Called every 15 seconds to check if any "connected" stations
     // are no longer sending data
     updateConnectedList() {
-        schedule.scheduleJob('0 * * * * *', function(){
+        schedule.scheduleJob('*/10 * * * * *', function(){
             checkConnected.checkConnected();
         }) 
-
-        schedule.scheduleJob('15 * * * * *', function(){
-            checkConnected.checkConnected();
-        })
-
-        schedule.scheduleJob('30 * * * * *', function(){
-            checkConnected.checkConnected();
-        })
-
-        schedule.scheduleJob('45 * * * * *', function(){
-            checkConnected.checkConnected();
-        })
     },
 
     //checks all user alerts every minute
